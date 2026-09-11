63f34183201c35b19d4c0a202916fb35f6620e4a
Marco Mese <mese@na.infn.it>
2026-09-11 12:52:06 +0200

    fixed user access

--- clkBoardPBR.srcs/sources_1/new/userAccess.vhd
+++ clkBoardPBR.srcs/sources_1/new/userAccess.vhd
@@ -29,41 +29,20 @@ end userAccess;
 architecture Behavioral of userAccess is
 
 signal usrAccessData : std_logic_vector(31 downto 0);
-signal usrAccessVal,
-       usrAccessSync : std_logic;
-signal usrAccessFF   : std_logic_vector(31 downto 0) := (others => '0');
 
 begin
-
-usrAccessOut <= usrAccessFF;
 
 usrAccessInst: USR_ACCESSE2
 port map(
     CFGCLK    => open,
     DATA      => usrAccessData,
-    DATAVALID => usrAccessVal
-);
-
-usrAccessValSyncInst: xpm_cdc_single
-generic map (
-    DEST_SYNC_FF   => 2,
-    INIT_SYNC_FF   => 1,
-    SIM_ASSERT_CHK => 0,
-    SRC_INPUT_REG  => 0
-)
-port map (
-    src_clk  => '0',
-    dest_clk => clk,
-    src_in   => usrAccessVal,
-    dest_out => usrAccessSync
+    DATAVALID => open
 );
 
 usrAccessProc: process(clk)
 begin
     if rising_edge(clk) then
-        if usrAccessSync = '1' then
-            usrAccessFF <= usrAccessData;
-        end if;
+        usrAccessOut <= usrAccessData;
     end if;
 end process;
 
